6ccf50b3c377a12f4b3647e27b7aaccee8810bcc
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-06-03 17:36:19 +0300

    add part of code

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -1,3 +1,11 @@
+import { Board } from "./Board";
+
 export class Cell{
-    
+    readonly x: number;
+    readonly y: number;
+    readonly coolor: Colors;
+    figures: Figure | null;
+    board: Board;
+    available: boolean; // can move on
+    id: number; // for react keys
 }
